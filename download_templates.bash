6267969be8de36a8b8757b586313be973185ae2a
Evgeny Mashkantsev <39212018+EvgenyMashkantsev@users.noreply.github.com>
2021-03-21 21:07:11 +0300

    Changed scripts for downloading LaTeX templates

--- download_templates.bash
+++ download_templates.bash
@@ -6,11 +6,10 @@ grep http latextemplates.html | grep --fixed-string '/cat/' >categories-list.txt
 cp ../../urllist.py urllist.py
 cp ../../gettemplatesfromcategory.bash gettemplatesfromcategory.bash
 python3 urllist.py 'categories-list.txt' > 'categories-url-list.txt'
-while IFS=, read -r line
+while read line
 do
-    bash gettemplatesfromcategory.bash "${line}"
+    bash -x gettemplatesfromcategory.bash "${line}"
 done < categories-url-list.txt
-wait
-rm -f categories-list.txt
-rm -f categories-url-list.txt
+# rm -f categories-list.txt
+# rm -f categories-url-list.txt
 echo "Check templates download result in $(pwd)"
--- gettemplatesfromcategory.bash
+++ gettemplatesfromcategory.bash
@@ -2,27 +2,30 @@
 compat50 2> /dev/null
 category=$1
 category="${category##*cat/}"
-mkdir "$category"
+mkdir "$(pwd)/${category}"
 cd "$category" || exit
 curl "https://www.latextemplates.com/${category}" > "${category}.html"
-grep http "${category}.html" | grep --fixed-string '/template/' > 'templates-list.txt'
+grep http "${category}.html" | grep --fixed-string '/template/' | uniq > 'templates-list.txt'
 cp ../urllist.py urllist.py
 python3 urllist.py 'templates-list.txt' > 'templates-url-list.txt'
 echo "Downloading templates from category: ${category}..."
-while IFS=, read -r templateurl
+echo "templates-url-list.txt is $(cat templates-url-list.txt)"
+while read templateurl
 do
+    echo "Templateurl is ${templateurl}"
     template="${templateurl##*template/}"
     echo "Downloading template: ${template}..."
     mkdir "$template"
     cd "$template" || exit
-    curl "https://www.latextemplates.com/template/${template}" > template_description.html
-    grep 'href' 'template_description.html' | grep --fixed-string 'zip' | grep -i -v 'overleaf' > 'zip_href.txt'
-    template2="${template%%href=\"*}"
-    template3="${template2##*\"}"
+    curl "${templateurl}" > template_description.html
+    grep 'href' 'template_description.html' | grep --fixed-string '.zip' | grep -i -v 'overleaf' > 'zip_href.txt'
+    ziphref="$(cat zip_href.txt)"
+    template2="${ziphref#*href=\"}"
+    template3="${template2%%\"*}"
     echo "${template3}" > 'zip_url_fragment.txt'
     echo "www.latextemplates.com/$(cat zip_url_fragment.txt)" > zip_url.txt
     wget -i zip_url.txt
-    find . -name "*.zip" | unzip
+    unzip *.zip
     # rm -f -- *.zip
     # rm -f zip_url.txt
     # rm -f zip_href.txt
