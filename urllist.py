279d8402003970737c4e21871f2ecd53e8ec7a6f
EvgenyMashkantsev <zande.com@gmail.com>
2021-03-31 19:52:36 +0300

    Removed broken stuff

--- urllist.py
+++
@@ -1,8 +0,0 @@
-import sys
-urllist = ''
-with open(sys.argv[1], 'rt') as file:
-    urllist += file.read()
-urllist = urllist.splitlines()
-for i in range(len(urllist)):
-    urllist[i] = urllist[i].split('https://',1)[1].split('"',1)[0]
-    print(urllist[i])
